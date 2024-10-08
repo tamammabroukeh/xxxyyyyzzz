45b88df38aa9bdf62b084a2d162ae2b97d9a5f97
tamammabroukeh <tamammb367@@gmail.com>
2024-10-07 21:25:51 -0700

    fix owner signin page

--- app/(auth)/owner/login/page.tsx
+++ app/(auth)/owner/login/page.tsx
@@ -1,5 +1,23 @@
-import SignIn from "@/components/auth/SignIn/SignIn";
+// import SignIn from "@/components/auth/SignIn/SignIn";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return <SignIn />;
+import { AuthButtons, AuthPageLayout, Heading } from "@/components/auth";
+import SignInForm from "@/components/auth/SignIn/SignInForm";
+import { SignInBackground } from "@/public";
+import useAuthAsServer from "@/hooks/useAuthAsServer";
+// import { getCookie } from "@/api/cookie";
+export default async function OwnerSignInpage() {
+  const session = await useAuthAsServer();
+
+  if (session) redirect("/");
+
+  // const data = JSON.parse((await getCookie("data"))!)
+  // console.log(data?.token);
+  return (
+    <AuthPageLayout alt="Background SignIn Page" img={SignInBackground}>
+      <Heading title="Sign in" />
+      <AuthButtons />
+      <SignInForm />
+    </AuthPageLayout>
+  );
 }
