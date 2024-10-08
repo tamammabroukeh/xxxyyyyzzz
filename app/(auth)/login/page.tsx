356787a891afc6f24349cb0a8b4ae3ad25263787
tamammabroukeh <tamammb367@@gmail.com>
2024-10-07 21:20:21 -0700

    fix error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,25 @@
-import SignIn from "@/components/auth/SignIn/SignIn";
+// import SignIn from "@/components/auth/SignIn/SignIn";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return <SignIn />;
+import { AuthButtons, AuthPageLayout, Heading } from "..";
+
+import SignInForm from "@/components/auth/SignIn/SignInForm";
+import { SignInBackground } from "@/public";
+import useAuthAsServer from "@/hooks/useAuthAsServer";
+// import { getCookie } from "@/api/cookie";
+export default async function SignInPage() {
+  const session = await useAuthAsServer();
+
+  if (session) redirect("/");
+
+  // const data = JSON.parse((await getCookie("data"))!)
+  // console.log(data?.token);
+
+  return (
+    <AuthPageLayout alt="Background SignIn Page" img={SignInBackground}>
+      <Heading title="Sign in" />
+      <AuthButtons />
+      <SignInForm />
+    </AuthPageLayout>
+  );
 }
